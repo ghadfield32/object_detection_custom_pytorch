ea7e032c62086f95d28c6da9aeca2401c69595fc
Geoffrey <ghadfield32@gmail.com>
2023-12-04 19:58:20 -0500

    new venv

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ghadf\vscode_projects\venv_projects\New folder\object_detection_custom_pytorch\.venv
+set VIRTUAL_ENV=C:\Users\ghadf\vscode_projects\venv_projects\Pytorch\object_detection_custom_pytorch-1\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
